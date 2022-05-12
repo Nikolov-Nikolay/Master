290a8cc97150220571d38d47c36bd15b70a01d07
Nikolov-Nikolay <105376975+Nikolov-Nikolay@users.noreply.github.com>
2022-05-12 16:39:20 +0300

    Delete FIrst Steps in Coding directory

--- FIrst Steps in Coding/08. Pet Shop.py
+++
@@ -1,9 +0,0 @@
-number_of_dogs_food = float(input())
-number_of_cats_food = int(input())
-price_for_1_pack_dogs_food = number_of_dogs_food * 2.50
-price_for_1_pack_cats_food = number_of_cats_food * 4
-final_price = price_for_1_pack_dogs_food + price_for_1_pack_cats_food
-
-print(f"{final_price} lv.")
-
-
