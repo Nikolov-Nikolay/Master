97a1a6110c703cbde739282d87a7bdb260b03463
Nikolov-Nikoloy <nnikolov.nikolay@gmail.com>
2022-05-16 13:43:51 +0300

    Programming Basics with Python

---
+++ Programing Basics with Python/Conditional Statements/05. Number 100...200.py
@@ -0,0 +1,12 @@
+# четем цяло число
+# няколко проверки
+# < 100 -> "less than 100"
+# между 100 и 200 -> 'Between 100 and 200'
+# > 200 -> 'Greater than 200'
+number = int(input())
+if number < 100:
+    print('Less than 100')
+elif 100 <= number <= 200:
+    print('Between 100 and 200')
+elif number > 200:
+    print('Greater than 200')
