aa59d7f7658669c96241228a57f6f569994a9ca9
Nikolov-Nikoloy <nnikolov.nikolay@gmail.com>
2022-05-17 12:23:44 +0300

    Programming Basics with Python

---
+++ Programing Basics with Python/02. Conditional Statements/12. Godzilla vs Kong.py
@@ -0,0 +1,15 @@
+budget = float(input())
+number_of_extras = int(input())
+price_for_cloting_for_one_extras = float(input())
+price_for_decor = budget * 0.10
+price_for_clothing = number_of_extras * price_for_cloting_for_one_extras
+if number_of_extras > 150:
+    price_for_clothing -= price_for_clothing * 0.10
+total_costs = price_for_decor + price_for_clothing
+diff = abs(budget - total_costs)
+if total_costs <= budget:
+    print('Action!')
+    print(f'Wingard starts filming with {diff:.2f} leva left.')
+else:
+    print('Not enough money!')
+    print(f'Wingard needs {diff:.2f} leva more.')
