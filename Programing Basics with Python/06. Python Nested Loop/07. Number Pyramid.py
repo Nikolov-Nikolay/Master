138494bf1fdd4bb79e441c6710c0c2ed1d61a4e8
Nikolov-Nikoloy <nnikolov.nikolay@gmail.com>
2022-05-24 10:43:50 +0300

    Programming Basics with Python

---
+++ Programing Basics with Python/06. Python Nested Loop/07. Number Pyramid.py
@@ -0,0 +1,15 @@
+number = int(input())
+current_number = 1
+condition = False
+for row in range(1, number + 1):
+    for col in range(1, row + 1):
+        if current_number > number:
+            condition = True
+            break
+
+        print(current_number, end=' ')
+        current_number += 1
+    if condition:
+        break
+    print()
+    
